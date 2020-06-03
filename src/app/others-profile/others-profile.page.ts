5be81565f75f4c97e9ef1519a4624c9ffb049a89
JunaidDev1 <51758839+JunaidDev1@users.noreply.github.com>
2020-06-03 23:48:01 +0500

    Profile scroll done

--- src/app/others-profile/others-profile.page.ts
+++ src/app/others-profile/others-profile.page.ts
@@ -9,7 +9,11 @@ export class OthersProfilePage implements OnInit {
 
   activeTab = 'activity';
   dummyItems: any = [1, 2, 3, 4, 5, 6, 7];
-
+  slideOpts = {
+    initialSlide: 0,
+    slidesPerView: 2.3
+  };
+  
   constructor() { }
 
   ngOnInit() {
